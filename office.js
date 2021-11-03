604f6e07d20ef7626cd7717735118ab15591873d
hsattar <39243545+hsattar@users.noreply.github.com>
2021-11-03 14:17:32 +0000

    added product using POST method

--- office.js
+++ office.js
@@ -1,1 +1,22 @@
 const myToken = config.TOKEN
+
+const addProductToServer = async e => {
+    e.preventDefault()
+
+    const productInfo = {
+        name: document.querySelector('#name').value,  
+        description: document.querySelector('#description').value,
+        brand: document.querySelector('#brand').value,
+        imageUrl: document.querySelector('#image').value,
+        price: document.querySelector('#price').value,
+    }
+
+    const response = await fetch('https://striveschool-api.herokuapp.com/api/product/', {
+        method: "POST",
+        body: JSON.stringify(productInfo),
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": myToken
+        }
+    })
+}
